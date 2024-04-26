3f7db192278a62e0de1a4ddae7041ab8a0f363ff
mbrochlips <m.brochlips@gmail.com>
2024-04-26 11:00:50 +0200

    fix

--- opg_numpy/script_numpy.py
+++ opg_numpy/script_numpy.py
@@ -1,28 +1,28 @@
-#Funktion der kan finder begge nulpunkterne af f.
+#funktion der kan finder begge nulpunkterne af f.
 
 import numpy as np
 
 def bisect(t_min,t_max,f,delta):
     t_mid = (t_min + t_max)/2
 
-    if (t_min - t_max)/2 < delta:
+    if (t_max - t_min)/2 < delta:
         return t_mid
     
-    if f[t_min] * f[t_mid] <= 0:
-        return bisect(t_min,t_mid,delta)
+    if f[t > t_min][0] * f[t > t_mid][0] <= 0:
+        return bisect(t_min,t_mid,f,delta)
     else:
-        return bisect(t_mid,t_max,delta)
+        return bisect(t_mid,t_max,f,delta)
 
-def find_f_eq_zero(f,t,delta):
-    zero_arr = []
-    t_min = np.min(t)
-    t_max = np.max(t)
-    tf_min = t[f.argmin()]
-    zero_arr.append(bisect(t_min,tf_min,f,delta))
-    zero_arr.append(bisect(tf_min,t_max,f,delta))
-    return zero_arr
+def find_arr_eq_zero(arr,t_min,t_max,delta):
+    root_arr = []
+    tf_min = t[arr.argmin()]
+    root_arr.append(bisect(t_min,tf_min,arr,delta=delta))
+    root_arr.append(bisect(tf_min,t_max,arr,delta=delta))
+    return root_arr
 
-t = np.linspace(0, 2 * np.pi, 1000)
-f = np.cos(t)
+t = np.linspace(0, 2 * np.pi, 10000)
+f_arr = np.cos(t)
+t_min = np.min(t)
+t_max = np.max(t)
 
-print(find_f_eq_zero(f,t,delta=0.1))
+find_arr_eq_zero(f_arr,t_min,t_max,delta=0.000001)
